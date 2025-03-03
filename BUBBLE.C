d7da38de270cf0f40e1e794f181063fe6c054113
karan soni <karanparekh1011@gmail.com>
2025-03-03 10:05:34 +0530

    Update BUBBLE.C

--- BUBBLE.C
+++ BUBBLE.C
@@ -1,3 +1,15 @@
+/*Bubble Sort is a simple sorting algorithm in Data Structures and Algorithms (DSA) that repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order. This process is repeated until the list is sorted.
+
+How Bubble Sort works:
+
+1. Start at the beginning of the list.
+2. Compare adjacent elements:
+  =>If the current element is greater than the next element, swap them.
+3. Move to the next pair of elements and repeat the comparison and swap.
+4. After each pass, the largest unsorted element bubbles up to its correct position at the end of the list.
+5. Repeat the process for the remaining unsorted portion of the list.
+6. Stop when no swaps are needed, indicating the list is sorted.*/
+
 #include<stdio.h>
 #include<conio.h>
 int main()
